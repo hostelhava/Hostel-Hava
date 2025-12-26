19bb3bb92e5b23a66dcfc4254ded96b7dc551213
yesyese <nagamohan765@gmail.com>
2025-12-26 15:01:38 +0530

    Update seed.js

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -1,6 +1,13 @@
 const { PrismaClient } = require("@prisma/client");
 const bcrypt = require("bcrypt");
-const prisma = new PrismaClient();
+require('dotenv').config();
+const prisma = new PrismaClient({
+    datasources: {
+        db: {
+            url: process.env.DATABASE_URL,
+        },
+    },
+});
 async function main() {
     const hashedPassword = await bcrypt.hash("admin123", 10);
     const admin = await prisma.admin.upsert({
