5301ce9053982a4ae1f8b52adc72bac8be80b0d4
yesyese <nagamohan765@gmail.com>
2025-12-26 11:36:11 +0530

    Added Structure for both backend and frontend

---
+++ client/src/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-red-600 text-white text-3xl font-bold underline text-center">
+      Hey Hi! Welcome to Hostel-Hava
+    </div>
+  )
+}
